2aeb81bb064b9389ca331e8af76022bdf2ce85da
sahithi <sahithipari99@gmail.com>
2021-07-08 12:05:21 +0530

    [sahithi] creating a borad

---
+++ TicTacToeGameProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TicTacToeGameProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Tic Tac Toe game");
+            TicTacToeGame game = new TicTacToeGame();
+            game.CreateBoard();
+        }
+    }
+}
